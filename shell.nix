47ea929e73ff4124437cf761eb0cd8e422b58f03
Anton Gushcha <ncrashed@protonmail.com>
2023-05-13 16:49:36 +0700

    Initial commit

---
+++ shell.nix
@@ -0,0 +1,16 @@
+with import ./nix/pkgs.nix {};
+let merged-openssl = symlinkJoin { name = "merged-openssl"; paths = [ openssl.out openssl.dev ]; };
+in stdenv.mkDerivation rec {
+  name = "rust-env";
+  env = buildEnv { name = name; paths = buildInputs; };
+
+  buildInputs = [
+    rustup
+    clang
+    llvm
+    llvmPackages.libclang
+    openssl
+    cacert
+    niv
+  ];
+}
